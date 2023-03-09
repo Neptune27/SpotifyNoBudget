16c4da5edf9b669bc305fbebf68b16daf6195f2c
thientranreal <thientt2612@gmail.com>
2023-03-09 22:56:39 +0700

    finish

---
+++ Thien/script/script.js
@@ -0,0 +1,12 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById('opacityForSideNav').style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById('opacityForSideNav').style.backgroundColor = "";
+}
+// End Side bar
