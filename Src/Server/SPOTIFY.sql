5861e2ecf2c5fd5b448b9b7ef1c9c1333e1c573c
ToKieu <94882501+ToKieu@users.noreply.github.com>
2023-03-27 15:52:24 +0700

    DATABASE V1.1
    các ID tui để sô nguên cho dễ nha

--- Src/Server/SPOTIFY.sql
+++ Src/Server/SPOTIFY.sql
@@ -2,221 +2,211 @@ drop database if exists spotify;
 create database spotify;
 use spotify;
 
+drop table if exists  ALBUM;
+ 
+drop table if exists AUTHOR_MUSIC;
+
+drop table if exists CREAT;
+
+drop table if exists FOLLOW;
+
+drop table if exists HISTORY;
+
+drop table if exists MUSIC;
+
+drop table if exists MUSIC_ALBUM;
+
+drop table if exists RECEIPT;
+
+drop table if exists SING;
+
+
+drop table if exists USER;
+
 /*==============================================================*/
 /* Table: ALBUM                                                 */
 /*==============================================================*/
-create table ALBUM
-(
-   ID_ALBUM             int not null,
-   NUMBER_SONG          int,
-   TIME                 time,
-   DATE                 date,
-   primary key (ID_ALBUM)
+create table ALBUM (
+ID_ALBUM integer not null,
+NUMBER_SONG integer,
+TIME time,
+DATE date,
+primary key (ID_ALBUM)
 );
 
-/*==============================================================*/
-/* Table: AUTHOR                                                */
-/*==============================================================*/
-create table AUTHOR
-(
-	
-   ID_USER              int not null,
-   NAME                 char(255),
-   PASSWORD             char(255),
-   GENDER               bool,
-   BIRTH                date,
-   COUNTRY              char(255),
-   EMAIL                char(255),
-   HAVE_PRIMIUM         bool,
-   TYPE                 int,
-   primary key (ID_USER)
-
-);
 
 /*==============================================================*/
 /* Table: AUTHOR_MUSIC                                          */
 /*==============================================================*/
-create table AUTHOR_MUSIC
-(
-   ID_USER              int not null,
-   ID_MUSIC             int not null,
-   primary key (ID_USER, ID_MUSIC)
+create table AUTHOR_MUSIC (
+AUT_USE_ID_USER integer not null,
+MUS_ID_MUSIC integer not null,
+primary key (AUT_USE_ID_USER, MUS_ID_MUSIC)
 );
 
 /*==============================================================*/
 /* Table: CREAT                                                 */
 /*==============================================================*/
-create table CREAT
-(
-   ID_USER              int not null,
-   ID_ALBUM             int not null,
-   primary key (ID_USER, ID_ALBUM)
+create table CREAT (
+USE_ID_USER integer not null,
+ALB_ID_ALBUM integer not null,
+primary key (USE_ID_USER, ALB_ID_ALBUM)
 );
 
 /*==============================================================*/
 /* Table: FOLLOW                                                */
 /*==============================================================*/
-create table FOLLOW
-(
-   USE_ID_USER          int not null,
-   ID_USER              int not null,
-   primary key (USE_ID_USER, ID_USER)
+create table FOLLOW (
+USE_ID_USER integer not null,
+USE_ID_USER2 integer not null,
+primary key (USE_ID_USER, USE_ID_USER2)
 );
 
 /*==============================================================*/
-/* Table: LIS_AL_MONTHLY                                        */
+/* Table: HISTORY                                               */
 /*==============================================================*/
-create table LIS_AL_MONTHLY
-(
-   ID_USER              int not null,
-   ID_ALBUM             int not null,
-   primary key (ID_USER, ID_ALBUM)
-);
-
-/*==============================================================*/
-/* Table: LIS_MUS_MONTHLY                                       */
-/*==============================================================*/
-create table LIS_MUS_MONTHLY
-(
-   ID_USER              int not null,
-   ID_MUSIC             int not null,
-   primary key (ID_USER, ID_MUSIC)
+create table HISTORY (
+ALB_ID_ALBUM integer not null,
+USE_ID_USER integer not null,
+MUS_ID_MUSIC integer not null,
+primary key (ALB_ID_ALBUM, USE_ID_USER, MUS_ID_MUSIC)
 );
 
 /*==============================================================*/
 /* Table: MUSIC                                                 */
 /*==============================================================*/
-create table MUSIC
-(
-   ID_MUSIC             int not null,
-   ID_ALBUM             int,
-   VIEW                 int,
-   TIME                 time,
-   primary key (ID_MUSIC)
+create table MUSIC (
+ID_MUSIC integer not null,
+VIEW integer,
+TIME time,
+LINK nvarchar(255),
+LINK_JSON nvarchar(255),
+primary key (ID_MUSIC)
 );
 
 /*==============================================================*/
-/* Table:  lyrics                                              */
+/* Table: MUSIC_ALBUM                                           */
 /*==============================================================*/
-create table LYRICS
-(
-   ID_MUSIC             int not null,
-   lyrics				char(255),-- CHỖ NÀY CÓ THỂ LƯU BÀI HÁT
-   TIME                 int,
-   primary key (ID_MUSIC)
+create table MUSIC_ALBUM (
+MUS_ID_MUSIC integer not null,
+ALB_ID_ALBUM integer not null,
+primary key (MUS_ID_MUSIC, ALB_ID_ALBUM)
 );
 
 /*==============================================================*/
 /* Table: RECEIPT                                               */
 /*==============================================================*/
-create table RECEIPT
-(
-   ID_RECEIPT           char(255) not null,
-   ID_USER              int not null,
-   DATE_BUY             date,
-   TOTAL_PRICE          int,
-   PAYMENT              char(255),
-   RETAILER             char(255),
-   ADDRESS              char(255),
-   VAT_NUMBER           int,
-   PRODUCT              char(255),
-   TOTAL_TAX            int,
-   TAX                  int,
-   PRICE                int,
-   primary key (ID_RECEIPT)
+create table RECEIPT (
+ID_RECEIPT nvarchar(255) not null,
+USE_ID_USER integer not null,
+DATE_BUY date,
+TOTAL_PRICE integer,
+PAYMENT nvarchar(255),
+RETAILER nvarchar(255),
+ADDRESS nvarchar(255),
+VAT_NUMBER integer,
+PRODUCT nvarchar(255),
+TOTAL_TAX integer,
+TAX integer,
+PRICE integer,
+primary key (ID_RECEIPT)
 );
 
 /*==============================================================*/
 /* Table: SING                                                  */
 /*==============================================================*/
-create table SING
-(
-   ID_USER              int not null,
-   ID_MUSIC             int not null,
-   primary key (ID_USER, ID_MUSIC)
+create table SING (
+SIN_USE_ID_USER integer not null,
+MUS_ID_MUSIC integer not null,
+primary key (SIN_USE_ID_USER, MUS_ID_MUSIC)
 );
 
+
 /*==============================================================*/
-/* Table: SINGER                                                */
+/* Table: USER                                                */
 /*==============================================================*/
-create table SINGER
-(
-   ID_USER              int not null,
-   NAME                 char(255),
-   PASSWORD             char(255),
-   GENDER               bool,
-   BIRTH                date,
-   COUNTRY              char(255),
-   EMAIL                char(255),
-   HAVE_PRIMIUM         bool,
-   TYPE                 int,
-   MONTHLY_LISTENER     int,
-   primary key (ID_USER)
+create table USER (
+ID_USER integer not null,
+NAME nvarchar(255),
+PASSWORD nvarchar(255),
+GENDER boolean,
+BIRTH date,
+COUNTRY nvarchar(255),
+EMAIL nvarchar(255),
+HAVE_PRIMIUM boolean,
+TYPE integer,
+MONTHLY_LISTENER integer,
+primary key (ID_USER)
 );
 
-/*==============================================================*/
-/* Table: USER                                                  */
-/*==============================================================*/
-create table USER
-(
-   ID_USER              int not null,
-   NAME                 char(255),
-   PASSWORD             char(255),
-   GENDER               bool,
-   BIRTH                date,
-   COUNTRY              char(255),
-   EMAIL                char(255),
-   HAVE_PRIMIUM         bool,
-   TYPE                 int,
-   primary key (ID_USER)
-);
 
-alter table AUTHOR add constraint FK_INHERITANCE_1 foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table AUTHOR_MUSIC
+   add foreign key FK_AUTH_AUTH_AUTH (AUT_USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table AUTHOR_MUSIC add constraint FK_AUTHOR_MUSIC foreign key (ID_USER)
-      references AUTHOR (ID_USER) on delete restrict on update restrict;
+alter table AUTHOR_MUSIC
+   add foreign key FK_AUTH_AUTH_MUSI (MUS_ID_MUSIC)
+      references MUSIC (ID_MUSIC)
+      on delete restrict;
 
-alter table AUTHOR_MUSIC add constraint FK_AUTHOR_MUSIC2 foreign key (ID_MUSIC)
-      references MUSIC (ID_MUSIC) on delete restrict on update restrict;
+alter table CREAT
+   add foreign key FK_CREA_CREA_USER (USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table CREAT add constraint FK_CREAT foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table CREAT
+   add foreign key FK_CREA_CREA_ALBU (ALB_ID_ALBUM)
+      references ALBUM (ID_ALBUM)
+      on delete restrict;
 
-alter table CREAT add constraint FK_CREAT2 foreign key (ID_ALBUM)
-      references ALBUM (ID_ALBUM) on delete restrict on update restrict;
+alter table FOLLOW
+   add foreign key FK_FOLL_FOLL_USER (USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table FOLLOW add constraint FK_FOLLOW foreign key (USE_ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table FOLLOW
+   add foreign key FK_FOLL_FOLL_USER (USE_ID_USER2)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table FOLLOW add constraint FK_FOLLOW2 foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table HISTORY
+   add foreign key FK_HIST_HIS__ALBU (ALB_ID_ALBUM)
+      references ALBUM (ID_ALBUM)
+      on delete restrict;
 
-alter table LIS_AL_MONTHLY add constraint FK_LIS_AL_MONTHLY foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table HISTORY
+   add foreign key FK_HIST_HIS__MUSI (MUS_ID_MUSIC)
+      references MUSIC (ID_MUSIC)
+      on delete restrict;
 
-alter table LIS_AL_MONTHLY add constraint FK_LIS_AL_MONTHLY2 foreign key (ID_ALBUM)
-      references ALBUM (ID_ALBUM) on delete restrict on update restrict;
+alter table HISTORY
+   add foreign key FK_HIST_USER_USER (USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table LIS_MUS_MONTHLY add constraint FK_LIS_MUS_MONTHLY foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table MUSIC_ALBUM
+   add foreign key FK_MUSI_MUSI_MUSI (MUS_ID_MUSIC)
+      references MUSIC (ID_MUSIC)
+      on delete restrict;
 
-alter table LIS_MUS_MONTHLY add constraint FK_LIS_MUS_MONTHLY2 foreign key (ID_MUSIC)
-      references MUSIC (ID_MUSIC) on delete restrict on update restrict;
+alter table MUSIC_ALBUM
+   add foreign key FK_MUSI_MUSI_ALBU (ALB_ID_ALBUM)
+      references ALBUM (ID_ALBUM)
+      on delete restrict;
 
-alter table MUSIC add constraint FK_MUSIC_ALBUM foreign key (ID_ALBUM)
-      references ALBUM (ID_ALBUM) on delete restrict on update restrict;
+alter table RECEIPT
+   add foreign key FK_RECE_BUY_USER (USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table RECEIPT add constraint FK_BUY foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
+alter table SING
+   add foreign key FK_SING_SING_SING (SIN_USE_ID_USER)
+      references USER (ID_USER)
+      on delete restrict;
 
-alter table SING add constraint FK_SING foreign key (ID_USER)
-      references SINGER (ID_USER) on delete restrict on update restrict;
+alter table SING
+   add foreign key FK_SING_SING_MUSI (MUS_ID_MUSIC)
+      references MUSIC (ID_MUSIC)
+      on delete restrict;
 
-alter table SING add constraint FK_SING2 foreign key (ID_MUSIC)
-      references MUSIC (ID_MUSIC) on delete restrict on update restrict;
-
-alter table SINGER add constraint FK_INHERITANCE_2 foreign key (ID_USER)
-      references USER (ID_USER) on delete restrict on update restrict;
-alter table LYRICS add constraint FK_HAVE_3 foreign key (ID_MUSIC)
-      references MUSIC (ID_MUSIC) on delete restrict on update restrict;
