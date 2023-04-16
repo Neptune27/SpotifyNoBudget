1032b11ef3cbd1dea0a518a76d69430bd597353b
thientranreal <thientt2612@gmail.com>
2023-04-16 21:22:59 +0700

    ... event listener

--- Src/Client/js/Queue.js
+++ Src/Client/js/Queue.js
@@ -18,10 +18,25 @@ class Queue {
                 <div class="queueTitle">Album Placeholder</div>
                 <div class="queueOther">
                     <span>3:44</span>
-                    <i class="fa-solid fa-ellipsis"></i>
+                    <i class="fa-solid fa-ellipsis three-dot"></i>
+                    <ul class="option rounded">
+                        <li>Option 1</li>
+                        <li>Option 2</li>
+                        <li>Option 3</li>
+                    </ul>
                 </div>
             </div>
         `;
+
+            // Click event for ...
+            for (let i = 0; i < this.nowPlayingQueueElems.children.length; i++) {
+                let child2 = this.nowPlayingQueueElems.children[i];
+                child2.querySelector('.three-dot').addEventListener("click", function (e) {
+                    e.stopPropagation();
+                    removeShowOption();
+                    this.nextElementSibling.classList.add('showOption');
+                });
+            }
         };
         this.setNextQueue = (nextQueue) => {
             let nQInner = "";
@@ -43,7 +58,12 @@ class Queue {
                 <div class="queueTitle">Akuma no ko</div>
                 <div class="queueOther">
                     <span>3:44</span>
-                    <i class="fa-solid fa-ellipsis"></i>
+                    <i class="fa-solid fa-ellipsis three-dot"></i>
+                    <ul class="option rounded">
+                        <li>Option 1</li>
+                        <li>Option 2</li>
+                        <li>Option 3</li>
+                    </ul>
                 </div>
             </div>
             `;
@@ -53,6 +73,13 @@ class Queue {
             for (let i = 0; i < this.nextQueueElems.children.length; i++) {
                 const child = this.nextQueueElems.children[i];
                 child.addEventListener("dblclick", () => this.setSong(i));
+
+                // Click event for ...
+                child.querySelector('.three-dot').addEventListener("click", function (e) {
+                    e.stopPropagation();
+                    removeShowOption();
+                    this.nextElementSibling.classList.add('showOption');
+                });
             }
         };
         this.setupQueue = (currentlyPlaying, nextQueue) => {
@@ -69,3 +96,14 @@ class Queue {
 }
 export { Queue };
 //# sourceMappingURL=Queue.js.map
+
+function removeShowOption() {
+    let showOption =  document.querySelector('.showOption');
+    if (showOption) {
+        showOption.classList.remove('showOption');
+    }
+}
+
+document.getElementById('mainQueue').addEventListener('click', e => {
+    removeShowOption();
+});
